9d854ca28dcaa83ce0e7fe1c15b932c3665e523b
lnylnylnylny <lnylnylnylny@naver.com>
2026-04-02 13:09:53 +0000

    initial commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
